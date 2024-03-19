c03218ad277750a8c1513a3b054acc41f29a241a
มาสเตอร์ อึ่ง <phrombutr@gmail.com>
2024-03-19 16:37:09 +0700

    finish 19mar

--- lib/states/main_home.dart
+++ lib/states/main_home.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:rutsproj/utility/app_controller.dart';
+import 'package:rutsproj/widgets/body_profile.dart';
+import 'package:rutsproj/widgets/body_service.dart';
+import 'package:rutsproj/widgets/widget_text.dart';
 
 class MainHome extends StatefulWidget {
   const MainHome({super.key});
@@ -13,11 +16,14 @@ class _MainHomeState extends State<MainHome> {
 
   AppController appController = Get.put(AppController());
 
-  var title = <String>['บริการ', 'โปรไฟร์',];
+  var titles = <String>['บริการ', 'โปรไฟร์',];
   var iconDatas = <IconData>[Icons.face_2, Icons.person,];
+  var bodys = <Widget>[const BodyService(), const BodyProfile(),];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Obx(
+       () => Scaffold(appBar: AppBar(title: WidgetText(data: titles[appController.indexBody.value]),),)
+    );
   }
 }
